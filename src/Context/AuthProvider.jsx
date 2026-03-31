2bf449f4b5b6d7096ea1c836b4a164fff2e10a33
advitya <advitya9@gmail.com>
2026-03-31 13:00:34 +0530

    UI fixes

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,27 +1,4 @@
-// import React, { createContext, useEffect, useState } from 'react'
-// import { getLocalStorage, setlocalStorage } from '../utils/localStorage';
-// export const AuthContext=createContext();
-// const AuthProvider = ({children}) => {
-//   const [userData, setuserData] = useState(null)
-//   useEffect(() => {
-//       // FIX: Only set dummy data if the database doesn't exist yet!
-//       if(!localStorage.getItem('employees') || !localStorage.getItem('admin')) {
-//           setlocalStorage();
-//       }
-//       const {employees, admin} = getLocalStorage();
-//       setuserData({employees, admin});
-//   }, [])
-//   return (
-//     <div>
-//         <AuthContext.Provider value={userData}>
-//             {children}
-//         </AuthContext.Provider>
-     
-//     </div>
-//   )
-// }
 
-// export default AuthProvider
 
 import React, { createContext, useEffect, useState } from 'react'
 import {getLocalStorage, setLocalStorage} from '../utils/localStorage'
